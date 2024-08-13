1c412d1ac8e46b25f66dce811ff1f6d4653bef2f
james.ho <jamesho86@gmail.com>
2024-08-13 22:28:57 +0800

    v1.0.0b27

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -20,7 +20,7 @@ window.addEventListener('load', async function (ev) {
     serviceWorkerSettings: {
       timeoutMillis: 40000,
       serviceWorkerUrl: "flutter_service_worker.js?version_worker=" + version + "?build_worker=" + build,
-      serviceWorkerVersion: "2444025769",
+      serviceWorkerVersion: "3633319491",
     },
   });
 });
